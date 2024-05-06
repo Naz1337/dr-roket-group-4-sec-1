1b7ac8628f1fd3fd12db28dc90a811b8aab7d1b6
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2024-05-06 14:22:14 +0800

    bootstrap ok dah
    bring bootstrap to main skjap

--- resources/views/bootstrap-test.blade.php
+++ resources/views/bootstrap-test.blade.php
@@ -9,7 +9,21 @@
 <body>
     <div class="container mt-4">
         <h1 class="mb-4">Welcome to Bootstrap Land in Laravel!</h1>
+<<<<<<< HEAD
         <button class="btn btn-primary">Let's go</button>
+=======
+        <button class="btn btn-primary mb-5">Let's go</button>
+        <form action="" class="">
+            <div class="form-floating">
+                <input type="text" class="form-control" placeholder="Search">
+                <label for="">Search</label>
+            </div>
+        </form>
+
+        <div>
+            <i class="bi bi-0-circle"></i>
+        </div>
+>>>>>>> d7aed85 (bootstrap ok dah)
     </div>
 </body>
 </html>
