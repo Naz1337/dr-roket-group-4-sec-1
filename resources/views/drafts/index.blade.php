e72f7e678886824bfd865324ccfc39b6699f3349
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2024-05-06 22:27:19 +0800

    cantikkan draft index page

--- resources/views/drafts/index.blade.php
+++ resources/views/drafts/index.blade.php
@@ -10,25 +10,28 @@
                 </button>
             </a>
         </div>
-        <table class="table table-hover">
+        <table class="table table-striped" style="width: 90%;">
             <thead class="table-light">
-            <tr>
+            <tr class="text-center">
                 <th>No.</th>
                 <th>Completion Date</th>
                 <th>Title</th>
                 <th>Page Count</th>
                 <th>Days taken to Complete</th>
-                <th>Actions</th>
+                <th class="text-center">Actions</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
+            <tr class="align-baseline">
                 <th>1.</th>
                 <td>11 September 2001</td>
                 <td>Analysis on Current Meta</td>
-                <td>188</td>
-                <td>9</td>
-                <td></td>
+                <td class="text-center">188</td>
+                <td class="text-end">9</td>
+                <td class="d-flex gap-2 justify-content-center">
+                    <a href="{{ route('drafts.show', ['draft' => 1]) }}" class="icon-link p-4"><i class="bi bi-eye"></i></a>
+                    <a href="{{ route('drafts.show', ['draft' => 1, 'download' => 1]) }}" class="icon-link p-4"><i class="bi bi-download"></i></a>
+                </td>
             </tr>
             </tbody>
         </table>
