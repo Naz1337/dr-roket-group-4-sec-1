498fa169700423a3c49e68273df831cb16e7fa8a
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2024-06-03 08:51:53 +0800

    woohoo we have seeder now

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\PlatinumSeeder;
+use Database\Seeders\UserProfileSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            UserProfileSeeder::class,
+            PlatinumSeeder::class,
         ]);
     }
 }
