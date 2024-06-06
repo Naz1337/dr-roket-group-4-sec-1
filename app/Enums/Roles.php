c27f2389c4b31a0b7456e78e62ada978fd74170c
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2024-06-06 19:08:37 +0800

    roles enum

---
+++ app/Enums/Roles.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Enums;
+
+class Roles
+{
+    const PLATINUM = 0;
+
+    // CRMP = 1
+    // Role Crmp don't exist.
+    const STAFF = 2;
+    const MENTOR = 3;
+
+    function getEnumValue($role) {
+        switch ($role) {
+            case 'platinum':
+                return Roles::PLATINUM;
+            case 'staff':
+                return Roles::STAFF;
+            case 'mentor':
+                return Roles::MENTOR;
+            default:
+                return null;
+        }
+    }
+}
