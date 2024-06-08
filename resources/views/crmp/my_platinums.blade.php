2cc961f71ffb15c0c1ff4b12d93b9e7201c6ebec
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2024-06-08 09:30:37 +0800

    Refactor platinum user display for profile views
    - Add hyperlink to profile
    - Consolidate class assignments
    - Adjust image and text layout
    - Ensure consistent styling
    - Clean up code structure

--- resources/views/crmp/my_platinums.blade.php
+++ resources/views/crmp/my_platinums.blade.php
@@ -11,16 +11,18 @@
                         <div class="col-4">
                             <div class="card shadow-lg">
                                 <div class="card-body d-flex flex-column justify-content-start pb-0">
-                                    <div class="d-flex align-items-start mb-4">
-                                        <img src="{{ \Illuminate\Support\Facades\Storage::url($platinum->plat_photo) }}" alt=""
-                                             class="rounded-circle me-3" style="max-height: 2.5rem">
-                                        <div>
-                                            <div class="form-label m-0">{{ $platinum->plat_name }}</div>
-                                            <div class="text-secondary" style="font-size: 0.875rem">
-                                                {{ $platinum->plat_edu_field }}
+                                    <a style="min-height: 3rem" class="mb-4" href="{{ route('view-profile-id', ['id' => $platinum->user->id]) }}">
+                                        <div class="d-flex align-items-start">
+                                            <img src="{{ \Illuminate\Support\Facades\Storage::url($platinum->plat_photo) }}" alt=""
+                                                 class="rounded-circle me-3" style="max-height: 2.5rem">
+                                            <div>
+                                                <div class="form-label m-0">{{ $platinum->plat_name }}</div>
+                                                <div class="text-secondary" style="font-size: 0.875rem">
+                                                    {{ $platinum->plat_edu_field }}
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    </a>
                                 </div>
                                 <div class="card-footer d-flex justify-content-center gap-3 align-items-center">
                                     <a class="btn btn-outline-primary" href="{{ route('crmp.view_draft_progress', ['platinum' => $platinum]) }}">Draft Progress</a>
