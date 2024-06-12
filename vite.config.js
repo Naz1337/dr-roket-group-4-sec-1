7e9adaa5088bfd2d3c227356483ef5bc04551d21
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2024-06-12 10:59:53 +0800

    deployment memery

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,13 @@ export default defineConfig({
                 'resources/js/draft_form.js',
                 'resources/js/draft_index.js',
                 'resources/js/draft_show.js',
+                'resources/scss/focus_index.scss',
+                'resources/js/focus_item_create.js',
+                'resources/js/focus_create.js',
+                'resources/js/ed_report.js',
+                'resources/js/draft_edit.js',
+                'resources/js/crmp_draft_progress.js',
+                'resources/js/crmp_index.js'
             ],
             refresh: true,
         }),
